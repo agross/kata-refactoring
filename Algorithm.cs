a574b4c0274c7c26a132807335b1f5515b03f4e1
Alexander Groß <agross@therightstuff.de>
2017-10-25 19:24:52 +0200

    extract methods

--- Algorithm.cs
+++ Algorithm.cs
@@ -33,55 +33,73 @@ namespace Algorithm
 
     public Pair Find(SearchType searchType)
     {
-      var temporaryList = new List<Pair>();
+      var durdations = CalculateDurationsBetweenThings(_list);
 
-      for(var i = 0; i < _list.Count - 1; i++)
-      {
-        for(var j = i + 1; j < _list.Count; j++)
-        {
-          var pair = new Pair();
-          if(_list[i].Date < _list[j].Date)
-          {
-            pair.First = _list[i];
-            pair.Second = _list[j];
-          }
-          else
-          {
-            pair.First = _list[j];
-            pair.Second = _list[i];
-          }
-          pair.Duration = pair.Second.Date - pair.First.Date;
-          temporaryList.Add(pair);
-        }
-      }
-
-      if(temporaryList.Count < 1)
+      if(durdations.Count < 1)
       {
         return new Pair();
       }
 
-      Pair answer = temporaryList[0];
-      foreach(var result in temporaryList)
+      var answer = FindShortestOrLongestDuration(searchType, durdations);
+
+      return answer;
+    }
+
+    static Pair FindShortestOrLongestDuration(SearchType searchType, List<Pair> durdations)
+    {
+      Pair answer = durdations[0];
+      foreach (var result in durdations)
       {
-        switch(searchType)
+        switch (searchType)
         {
           case SearchType.ShortestDuration:
-            if(result.Duration < answer.Duration)
+            if (result.Duration < answer.Duration)
             {
               answer = result;
             }
             break;
 
           case SearchType.LongestDuration:
-            if(result.Duration > answer.Duration)
+            if (result.Duration > answer.Duration)
             {
               answer = result;
             }
             break;
         }
       }
-
       return answer;
+    }
+
+    static List<Pair> CalculateDurationsBetweenThings(List<Thing> things)
+    {
+      var temporaryList = new List<Pair>();
+
+      for (var i = 0; i < things.Count - 1; i++)
+      {
+        for (var j = i + 1; j < things.Count; j++)
+        {
+          var pair = EarliestFirst(things[i], things[j]);
+          pair.Duration = pair.Second.Date - pair.First.Date;
+          temporaryList.Add(pair);
+        }
+      }
+      return temporaryList;
+    }
+
+    static Pair EarliestFirst(Thing left, Thing right)
+    {
+      var pair = new Pair();
+      if (left.Date < right.Date)
+      {
+        pair.First = left;
+        pair.Second = right;
+      }
+      else
+      {
+        pair.First = right;
+        pair.Second = left;
+      }
+      return pair;
     }
   }
 }
